659d9d11c78b71e3edb32736f7c903455250668f
duongspm <tranvanduong10a22016bb@gmail.com>
2022-03-28 14:12:36 +0700

    update 2: sửa tên thành Lolycuptea

--- loly-cup-tea/src/App.js
+++ loly-cup-tea/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Loly Cup Tea
         </a>
       </header>
     </div>
